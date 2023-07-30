c150023f618e1f252f4ff46bce4945f8603f0688
Andrey Saksonov <asaksonov@gmail.com>
2023-07-30 21:09:44 +0300

    add modul tests and add information about func

--- utils.py
+++ utils.py
@@ -6,22 +6,24 @@ from time import strftime
 def get_data():
     """
     Функция возвращает список транзакций из JSON
-    :return: список
     """
     with open('operations.json', 'r', encoding='utf8') as f:
         data = json.load(f)
     return data
 
 def get_filtered_data(data):
+    """Функция фильтрует список по параметру EXECUTED"""
     data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
     return data
 
 def get_last_values(data, count_last_values):
+    """Функция сортирует список от самой последней операции. Выводит заданное количество операций"""
     data = sorted(data, key=lambda x: x['date'], reverse=True)
     data = data[:count_last_values]
     return data
 
 def get_formatted_data(data):
+    """Функция формирует нужные сообщения для вывода"""
     formatted_date = []
     for row in data:
         normalized_date = date.fromisoformat(row["date"][:10])
